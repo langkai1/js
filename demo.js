bd352467addfbf9670e39dff6003c427986b35d1
langkai1 <253976722@qq.com>
2018-04-13 19:16:37 +0800

    完成

---
+++ demo.js
@@ -0,0 +1,20 @@
+/*
+* @Author: dell
+* @Date:   2018-03-30 15:14:19
+* @Last Modified by:   dell
+* @Last Modified time: 2018-03-30 16:21:12
+*/
+
+alert(666);
+console.log(1);
+prompt("please input a message");
+var a=12345678;
+console.log(a);
+console.log(typeof a);
+var b="i am china.";
+console.log(b);
+console.log(typeof b);
+var h1=document.querySelector("h1");
+console.log(h1);
+var txet=h1.innerHTML;
+h1.innerHTML="21212121";
